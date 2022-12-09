f916bf91bede74398b063d79104c0ea4edd92dde
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-09 04:59:29 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multiligual puzzle");
 		return (0);
-
 }
