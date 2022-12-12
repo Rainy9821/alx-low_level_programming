9c666ad41ee90f51f15a1892e2ea13352f13d55a
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-12 01:07:08 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,9 @@
 int main(void)
 
 {
+
+	char c;
+	
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
