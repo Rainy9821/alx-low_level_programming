f54c646336c3291f52ab5808acfa116306ba89fb
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-13 00:43:03 -0800

    numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return integer from 0-10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
