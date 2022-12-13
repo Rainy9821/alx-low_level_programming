2b288653a3cae1c705445bfd08419630166aadfa
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-13 01:33:26 -0800

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - return all possible combinations of single digits numbers seperated by comma followed by space
+ * main - prints number from 0-9 seperated by comma followed by space
  *
  * Return: Always 0 (success)
+ *
  */
 
 int main(void)
 
 {
 	int num = '0';
-	while (num <= '9');
 
+	while (num <= '9');
 	{
 		putchar(num);
 		if  (num != '9')
@@ -21,8 +22,6 @@ int main(void)
 		}
 		num++;
 	}
-
 	putchar('\n');
 	return (0);
-
 }
