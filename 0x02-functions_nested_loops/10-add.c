1b7b5a13cf337878386e39caf1cbbc6fb47265f5
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-14 05:57:18 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point for addition
+ * @f: one value to be added
+ * @g: second value to be added
+ * Return: Always 0
+ */
+
+int add(int f, int g)
+
+{
+	return (f + g);
+}
