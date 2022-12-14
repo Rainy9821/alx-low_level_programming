143d56e4b99f3e8aa66b60f5458f15e2d968cd21
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-14 05:47:00 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,6 +8,7 @@
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -23,7 +24,7 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10/  '0');
+					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
