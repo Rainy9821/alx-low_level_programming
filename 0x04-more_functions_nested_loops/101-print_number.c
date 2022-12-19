d33c6949d430c423201a15bfbe18759bf69e15a1
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-19 05:55:59 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,35 @@
 #include "main.h"
 
 /**
- * main - print prime
- * Return: 0
+ * print_number - prints an integer
+ * @n: param
  */
-int main(void)
-{
-	unsigned long int i = 3, n = 612852475143;
 
-	for (; i < 12057; i += 2)
+void print_number(int n)
+{
+	unsigned int i, j, count;
+
+	if (n < 0)
 	{
-		while (n % i == 0 && n != i)
-			n /= i;
+		_putchar(45);
+		i = n * -1;
 	}
-	printf("%lu\n", n);
-	return (0);
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
 }
