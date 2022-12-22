ab20852e6c82ecf44cc174ff6e14934f48944b52
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-22 05:58:10 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string
  * separators of words are: space, tabulation
@@ -8,13 +7,12 @@
  *
  * Return: Pointer to s
  */
-
 char *cap_string(char *s)
 {
 	int count;
 
 	count = 0;
- while (s[count] != '\0')
+  while (s[count] != '\0')
  	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
