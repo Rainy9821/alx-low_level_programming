4dc1c890f738693b0bcec9452ac6c18765155675
Rainy9821 <ezekielchukwuemeka.orie@gmail.com>
2022-12-23 01:51:05 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,29 @@
 #include "main.h"
+
 /**
- * cap_string - capitalizes every first letter of a word in a string
- * separators of words are: space, tabulation
- * new line
+ * cap_string - capitalizes all words of  a string
+ * seperators of words: space, tabulation, new line
  * @s: pointer to string
- *
- * Return: Pointer to s
+ * Return: pointer to s
  */
+
 char *cap_string(char *s)
 {
 	int count;
 
 	count = 0;
-  while (s[count] != '\0')
-  	{
+
+	while (s[count] != '\0')
+	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
 			s[0] = s[0] - 32;
 		}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
-    		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-    		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-    		    || s[count] == '{' || s[count] == '}')
+			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+			|| s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
@@ -31,5 +32,5 @@ char *cap_string(char *s)
 		}
 		count++;
 	}
-  	return (s);
+	return(s);
 }
